0a456151712b7dd6c55d0b916e7875bea6fe1b4b
arekmula <arekmula97@gmail.com>
2020-12-29 01:03:00 +0100

    TF - Added function that splits first dataset

--- tf_implementation/segmentation/dataset/__init__.py
+++ tf_implementation/segmentation/dataset/__init__.py
@@ -1,1 +1,1 @@
-from .file_processing import load_raw_volume, load_labels_volume, save_labels
+from .file_processing import load_raw_volume, load_labels_volume, save_labels, split_first_dataset
--- tf_implementation/segmentation/dataset/file_processing.py
+++ tf_implementation/segmentation/dataset/file_processing.py
@@ -1,8 +1,9 @@
-from pathlib import Path
-from typing import Tuple
-
 import nibabel as nib
 import numpy as np
+
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+from typing import Tuple
 
 
 def load_raw_volume(path: Path) -> Tuple[np.ndarray, np.ndarray]:
@@ -37,3 +38,30 @@ def save_labels(data: np.ndarray, affine: np.ndarray, path: Path):
     :return:
     """
     nib.save(nib.Nifti1Image(data, affine), str(path))
+
+
+def split_first_dataset(train_set_path: Path):
+    """
+    Function that splits train set from 1st dataset into train and validation set
+    :param train_set_path: path to folder containing train scans where labels and scans are in the same directory
+    :return train_scans: list of tuples containing path for train scan and path for corresponding mask
+    :return val_scans: list of tuples containing path for validation scan and path for corresponding mask
+    """
+
+    scan_list = []  # List of tuples containing scans and their masks
+    file_extension = ".nii.gz"
+    mask_partial_filename = "_mask"
+
+    for scan_full_path in train_set_path.iterdir():
+        scan_filename = scan_full_path.name[:-len(file_extension)]  # Deleting .nii.gz from file name
+        if mask_partial_filename not in scan_filename:
+            scan_mask_filename = scan_filename + mask_partial_filename + file_extension
+            scan_mask_full_path = scan_full_path.parent / Path(scan_mask_filename)
+            scan_list.append((scan_full_path, scan_mask_full_path))
+
+    train_scans, val_scans = train_test_split(scan_list, random_state=42, train_size=0.8)
+
+    print(f"Number of train scans from first dataset: {len(train_scans)}")
+    print(f"Number of validation scans from first dataset: {len(val_scans)}")
+
+    return train_scans, val_scans
