c187a1111251d73be00baf1571ed12a2aeba7c84
arekmula <arekmula97@gmail.com>
2020-12-29 18:49:31 +0100

    TF - Added generator for val scans

--- tf_implementation/experiments/show_slices_from_generator.py
+++ tf_implementation/experiments/show_slices_from_generator.py
@@ -6,16 +6,18 @@ from segmentation.dataset import scans_generator
 
 
 def main(args):
-    path = Path(args.path)
+    train_path = Path(args.train_dir_path)
+    val_path = Path(args.val_dir_path)
 
-    scans = scans_generator(path)
+    train_scans, val_scans = scans_generator(train_path, val_path)
 
     # Get batch of data
-    images, masks = next(scans)
+    images, masks = next(train_scans)
 
     for i in range(len(images)):
         fig = plt.figure()
         fig.add_subplot(1, 2, 1)
+        print("Please note that preprocessing for specific backbone might cause strange image for human eye")
         plt.imshow(images[i], cmap="gray")
 
         fig.add_subplot(1, 2, 2)
@@ -26,7 +28,8 @@ def main(args):
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--path", metavar="path", type=str, required=True)
+    parser.add_argument("--train_dir_path", metavar="train_dir_path", type=str, required=True)
+    parser.add_argument("--val_dir_path", metavar="val_dir_path", type=str, required=True)
 
     args, _ = parser.parse_known_args()
 
--- tf_implementation/segmentation/dataset/dataset.py
+++ tf_implementation/segmentation/dataset/dataset.py
@@ -1,42 +1,76 @@
-from pathlib import Path
 import tensorflow as tf
 
+from pathlib import Path
+from segmentation_models import get_preprocessing
 
-def scans_generator(scans_directory_path: Path, batch_size=16, target_size=(200, 200), shuffle=True):
+
+def scans_generator(scans_train_directory_path: Path, scans_val_directory_path: Path, batch_size=16,
+                    target_size=(200, 200), shuffle=True, backbone="efficientnetb0"):
     """
+    Tensorflow dataset generator.
 
+    :param scans_train_directory_path: It should contain one subdirectory per class. Any PNG, JPG, BMP, PPM or TIF
+    images inside each of the subdirectories directory tree will be included in the generator.
+    :param scans_val_directory_path: It should contain one subdirectory per class. Any PNG, JPG, BMP, PPM or TIF
+    images inside each of the subdirectories directory tree will be included in the generator.
+    :param backbone: backbone name of network you will use
     :param shuffle: Whether to shuffle the data (default: True) If set to False, sorts the data in alphanumeric order.
     :param target_size: The dimensions to which all images found will be resized.
-    :param scans_directory_path: It should contain one subdirectory per class. Any PNG, JPG, BMP, PPM or TIF images
-     inside each of the subdirectories directory tree will be included in the generator
     :param batch_size: Size of the batches of data
-    :return joined generator: A DirectoryIterator yielding tuples of (x, y) where x is a numpy array containing a batch
-     of images with shape (batch_size, *target_size, channels) and y is a numpy array of corresponding labels
+    :return train_combined_generator: A DirectoryIterator yielding tuples of (x, y) where x is a numpy array containing
+    a batch of images with shape (batch_size, *target_size, channels) and y is a numpy array of corresponding labels
+    :return val_combined_generator: A DirectoryIterator yielding tuples of (x, y) where x is a numpy array containing
+    a batch of images with shape (batch_size, *target_size, channels) and y is a numpy array of corresponding labels
     """
 
+    # Preprocess image based on backbone implementation
+    preprocess_input = get_preprocessing(backbone)
+
+    # TODO: Check augmentations
     images_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-        rescale=1/255
+        # rescale=1/255, # -> Uncomment if not using preprocess_input
+        preprocessing_function=preprocess_input
     )
 
     masks_datagen = tf.keras.preprocessing.image.ImageDataGenerator()
 
-    input_images_generator = images_datagen.flow_from_directory(directory=scans_directory_path/"scans",
+    train_images_generator = images_datagen.flow_from_directory(directory=scans_train_directory_path / "scans",
                                                                 target_size=target_size,
                                                                 batch_size=batch_size,
                                                                 seed=42,
                                                                 shuffle=shuffle,
                                                                 class_mode=None,
-                                                                interpolation="nearest",
-                                                                color_mode="grayscale")
+                                                                interpolation="bilinear",
+                                                                )
 
-    input_masks_generator = masks_datagen.flow_from_directory(directory=scans_directory_path/"labels",
+    train_masks_generator = masks_datagen.flow_from_directory(directory=scans_train_directory_path / "labels",
                                                               target_size=target_size,
                                                               batch_size=batch_size,
                                                               seed=42,
                                                               shuffle=True,
                                                               class_mode=None,
-                                                              interpolation="nearest")
+                                                              interpolation="nearest",
+                                                              )
 
-    joined_generator = zip(input_images_generator, input_masks_generator)
+    val_images_generator = images_datagen.flow_from_directory(directory=scans_val_directory_path / "scans",
+                                                              target_size=target_size,
+                                                              batch_size=batch_size,
+                                                              seed=42,
+                                                              shuffle=shuffle,
+                                                              class_mode=None,
+                                                              interpolation="bilinear",
+                                                              )
 
-    return joined_generator
+    val_masks_generator = masks_datagen.flow_from_directory(directory=scans_val_directory_path / "labels",
+                                                            target_size=target_size,
+                                                            batch_size=batch_size,
+                                                            seed=42,
+                                                            shuffle=True,
+                                                            class_mode=None,
+                                                            interpolation="nearest",
+                                                            )
+
+    train_combined_generator = zip(train_images_generator, train_masks_generator)
+    val_combined_generator = zip(val_images_generator, val_masks_generator)
+
+    return train_combined_generator, val_combined_generator
